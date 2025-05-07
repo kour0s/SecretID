692fb1bf65f10bb77ef11b4a6845401ab5ebd60d
kour0s <129988843+kour0s@users.noreply.github.com>
2025-05-07 12:25:03 +0200

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100 text-xl">
+      👋 Hello depuis SecretID
+    </div>
+  );
+}
+
+export default App;
