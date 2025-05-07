04fc08aef75ba6f7b81f3e8752e2e6067804ad19
kour0s <129988843+kour0s@users.noreply.github.com>
2025-05-07 12:23:42 +0200

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-100 text-xl">
-      👋 Hello depuis SecretID
-    </div>
-  );
-}
-
-export default App;
